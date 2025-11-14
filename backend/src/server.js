9200c5e5f7c8f7dfaa0fad6ce21d08a10b59bbbd
sauravpatel77s <sauravpatel220930@acropolis.in>
2025-11-14 20:10:10 +0530

    backend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -18,13 +19,21 @@ app.get("/books", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
-const PORT = ENV.PORT;
 
-app.listen(ENV.PORT, () => {
-  console.log("server is running on PORT : ", ENV.PORT);
-});
+
+const startServer = async ()=>{
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {console.log("server is running on PORT : ", ENV.PORT)});
+  } catch (error) {
+    console.error("Error starting the server", error);
+    
+  }
+};
+
+startServer();
